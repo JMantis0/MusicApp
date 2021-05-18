879c9148fae9765d9a3fa1a4fbce79d83cfe5840
JMan <60026263+JMantis0@users.noreply.github.com>
2021-05-18 14:31:38 -0700

    Removed bad project and added a good one

--- client/src/MusicApp.test.tsx
+++ client/src/MusicApp.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+
+
 import MusicApp from './MusicApp';
 
 test('renders learn react link', () => {
