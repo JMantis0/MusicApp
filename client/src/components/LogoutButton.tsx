baab31d211cf6300e8293c616f603dfdea5325e1
JMan <60026263+JMantis0@users.noreply.github.com>
2021-05-18 18:42:29 -0700

    imported useHistory and useAppDispatchHooks. Added logOut function and added to onClick of button.  logOut function dispatches call to resetCurrentUserState and push url to landing page

--- client/src/components/LogoutButton.tsx
+++ client/src/components/LogoutButton.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import { useAppDispatch } from "../redux/hooks";
+import { resetCurrentUserState } from "../redux/musicAppSlice";
+import { useHistory } from "react-router-dom";
 
 const LogoutButton = () => {
-  return <Button>Logout</Button>;
+  const dispatch = useAppDispatch();
+  const history = useHistory();
+
+  const logOut = () => {
+    dispatch(resetCurrentUserState());
+    history.push("/");
+  };
+  return <Button onClick={logOut}>Logout</Button>;
 };
 
 export default LogoutButton;
