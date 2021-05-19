4dc29d5e673fc3f2b5c44344da5e7448ac8176e2
JMan <60026263+JMantis0@users.noreply.github.com>
2021-05-18 17:33:54 -0700

    Imported selector function, dispatcher function, and setFavoritesState reducer into AddToFavoritesButton component.  Added redux statehook and dispatch hook to component.  Added explanatory comments to be filled in with code later.

--- client/src/components/AddToFavoritesButton.tsx
+++ client/src/components/AddToFavoritesButton.tsx
@@ -1,20 +1,37 @@
 import React from "react";
 import axios from "axios";
+import { selectMusicApp } from "../redux/musicAppSlice";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
 
 const AddToFavoritesButton = () => {
-  const saveToFavorites = () => {
+  const musicAppState = useAppSelector(selectMusicApp);
+  const dispatch = useAppDispatch();
+
+  //  Function requests back end to update user's favorites in the db, then
+  //  updates client state to reflect the new favorite.
+  const saveToFavorites = (event:any) => {
+    console.log("Save to favorites button clicked.  The event is: ", event);
     // Perform call to Database here.  Save the Currently Selected song
     // to the current user's favorites.
     const data = {
+      //The properties for the 'favorite' to add go here, to be sent in post request.
+      //  The favorite is to be saved in the db
     }
     axios
       .post("/add_song_to_favorites", data)
       .then((response) => {
         console.log("response", response);
-        //update the state here to reflect change in backend
+        const newFavorite = response.data
+        //update the state here to reflect that the favorite was added in backend
+        //Something like:
+        // ***
+        // dispatch(setFavoritesState(newFavorite));
+        // ***
+
       })
       .catch((error) => {
-        console.log("there was an error", error);
+        //  Error output for debugging
+        console.log("There was an error adding to favorite", error);
       });
   };
   return (
