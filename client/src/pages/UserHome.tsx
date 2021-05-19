c545a8e08f167bd9ab1c77e4bad84cbbea1b21d4
JMan <60026263+JMantis0@users.noreply.github.com>
2021-05-18 18:14:36 -0700

    Add imports for MUI Grid component, MusicApp.module.css, and SearchInput.  Created a header section and divided them with grid items, placing header in first column and a logoutbutton in the second column.

--- client/src/pages/UserHome.tsx
+++ client/src/pages/UserHome.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import SearchInput from "../components/SearchInput"
-
+import SearchInput from "../components/SearchInput";
+import Grid from "@material-ui/core/Grid";
+import style from "../MusicApp.module.css";
 
 const UserHome = () => {
-  return <div>This will be the home page
-<SearchInput />
-  </div>;
+  return (
+    <div>
+      <header>
+        <Grid container>
+          <Grid xs={10} item className={style.center}>
+            <h1 >User Home</h1>
+          </Grid>
+
+          <Grid xs={2} item className={style.center}>
+            <button>Logout</button>
+          </Grid>
+        </Grid>
+      </header>
+      <SearchInput />
+    </div>
+  );
 };
 export default UserHome;
