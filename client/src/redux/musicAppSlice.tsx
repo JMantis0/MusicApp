fa1c45e0580e23a4de8c383f3befc47a479bcf20
JMan <60026263+JMantis0@users.noreply.github.com>
2021-05-19 21:34:36 -0700

    Updated MusicAppState interface to utilize pojo classes and class types.  also Added setter reducer functions, which expect a specific type on action.payload

--- client/src/redux/musicAppSlice.tsx
+++ client/src/redux/musicAppSlice.tsx
@@ -8,73 +8,74 @@ import {
   //  AppThunk
 } from "./store";
 
+import { Album, Artist, Playlist, Track, User } from "./pojos";
+
 export interface MusicAppState {
-  currentUserState: {
-    firstName: string;
-    lastName: string;
-    userID: string;
-    password: string;
-    id: { timestamp: number | null; date: number | null };
-  };
-  favoritesState: Array<{}>;
-  searchInputState: string;
-  loginFormState: { userID: string; password: string };
+  album: typeof Album | null;
+  artist: typeof Artist | null;
+  playlist: typeof Playlist | null;
+  track: typeof Track | null;
+  user: typeof User | null;
+  searchInput: string | null;
+  loginForm: { username: string | null; password: string | null };
 }
 
 const initialState: MusicAppState = {
-  loginFormState: { userID: "", password: "" },
-  currentUserState: {
-    firstName: "",
-    lastName: "",
-    userID: "",
-    password: "",
-    id: { timestamp: null, date: null },
-  },
-  favoritesState: [],
-  searchInputState: "",
+  album: null,
+  artist: null,
+  playlist: null,
+  track: null,
+  user: null,
+  searchInput: null,
+  loginForm: { username: null, password: null },
 };
 
 export const musicAppSlice = createSlice({
   name: "musicApp",
   initialState,
   reducers: {
-    setLoginFormState: (state, action) => {
+    setAlbum: (state, action: { payload: typeof Album }) => {
+      state.album = action.payload;
+    },
+    setArtist: (state, action: { payload: typeof Artist }) => {
+      state.artist = action.payload;
+    },
+    setPlaylist: (state, action: { payload: typeof Playlist }) => {
+      state.playlist = action.payload;
+    },
+    setTrack: (state, action: { payload: typeof Track }) => {
+      state.track = action.payload;
+    },
+    setUser: (state, action: { payload: typeof User }) => {
+      state.user = action.payload;
+    },
+    logoutUser: (state) => {
+      state.user = null;
+    },
+    setLoginForm: (
+      state,
+      action: { payload: { fieldName: string; value: string } }
+    ) => {
       const fieldName = action.payload.fieldName;
       const value = action.payload.value;
-      state.loginFormState = { ...state.loginFormState, [fieldName]: value };
+      state.loginForm = { ...state.loginForm, [fieldName]: value };
     },
-    resetLoginFormState: (state) => {
-      state.loginFormState.userID = "";
-      state.loginFormState.password = "";
+    resetLoginForm: (state) => {
+      state.loginForm.username = null;
+      state.loginForm.password = null;
     },
-    setCurrentUserState: (state, action) => {
-      state.currentUserState = { ...state.currentUserState, ...action.payload };
-    },
-    resetCurrentUserState: (state) => {
-      state.currentUserState = {
-        firstName: "",
-        lastName: "",
-        userID: "",
-        password: "",
-        id: { timestamp: null, date: null },
-      };
-    },
-    setSearchInputState: (state, action) => {
-      state.searchInputState = action.payload;
-    },
-    setFavoritesState: (state, action) => {
-      state.favoritesState = { ...state.favoritesState, ...action.payload };
+    setSearchInput: (state, action: { payload: string }) => {
+      state.searchInput = action.payload;
     },
   },
 });
 
 export const {
-  setCurrentUserState,
-  resetCurrentUserState,
-  setSearchInputState,
-  setFavoritesState,
-  setLoginFormState,
-  resetLoginFormState,
+  setLoginForm,
+  resetLoginForm,
+  setUser,
+  logoutUser,
+  setSearchInput,
 } = musicAppSlice.actions;
 
 export const selectMusicApp = (state: RootState) => state.musicApp;
