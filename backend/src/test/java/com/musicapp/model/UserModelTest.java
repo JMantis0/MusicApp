12f7605f81bec9632b273624e4ca0829809a2436
jcowelch <jcowelch@gmail.com>
2021-05-20 12:09:10 -0400

    User Model tests (47%)

--- backend/src/test/java/com/musicapp/model/UserModelTest.java
+++ backend/src/test/java/com/musicapp/model/UserModelTest.java
@@ -1,4 +1,49 @@
 package com.musicapp.model;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 public class UserModelTest {
+
+    private User user;
+
+    @BeforeEach
+    public void setUp(){
+        user = new User();
+        user.setUserId("Id");
+        user.setFirstName("FN");
+        user.setLastName("LN");
+        user.setUsername("UN");
+        user.setPassword("PW");
+    }
+
+    @Test
+    public void userIDTest(){
+        String foundID = user.getUserId();
+        Assert.assertEquals("Id",foundID);
+    }
+
+    @Test
+    public void userFirstNameTest(){
+        String foundFirstName = user.getFirstName();
+        Assert.assertEquals("FN",foundFirstName);
+    }
+
+    @Test
+    public void userLastNameTest(){
+        String foundLastName = user.getLastName();
+        Assert.assertEquals("LN",foundLastName);
+    }
+
+    @Test
+    public void userUsernameTest(){
+        String foundUsername = user.getUsername();
+        Assert.assertEquals("UN",foundUsername);
+    }
+
+    @Test
+    public void userPasswordTest(){
+        String foundPassword = user.getPassword();
+        Assert.assertEquals("PW",foundPassword);
+    }
 }
