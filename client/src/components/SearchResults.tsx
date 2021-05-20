abf7c2fc3298e1d5af1b99b9a4761d522ce744e2
JMan <60026263+JMantis0@users.noreply.github.com>
2021-05-20 09:33:53 -0700

    Removed unused import

--- client/src/components/SearchResults.tsx
+++ client/src/components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useMemo } from "react";
+import React from "react";
 import Button from "@material-ui/core/Button";
 import axios from "axios";
 //  import redux state hooks
