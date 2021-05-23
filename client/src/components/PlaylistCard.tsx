e777102ae7bf2031711c0618261d38d57a2382d4
JMan <60026263+JMantis0@users.noreply.github.com>
2021-05-22 21:10:56 -0700

    passed prop to card to display playlistname

--- client/src/components/PlaylistCard.tsx
+++ client/src/components/PlaylistCard.tsx
@@ -1,45 +1,41 @@
-import React,{useState, useEffect} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 //  import redux state hooks
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 //  import reducer and state selector
-import {selectMusicApp, setPlaylists} from '../redux/musicAppSlice'
-import { Playlist } from "../redux/pojos";
+import { selectMusicApp, setPlaylists } from "../redux/musicAppSlice";
 // interface PlaylistProps{
 //     playlist: Playlist;
 // }
 
-const PlaylistCard = () => {
-    return (
-        <Card>
-            <CardActionArea>
-                <CardMedia/>
-                <CardContent>
-                    <Typography variant="body2" 
-                                color="textSecondary"
-                    >
-                        Playlist Name
-                    {/*{playlistName}*/}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions>
-                <Button size="small" color="primary">
-                    View Playlist
-                </Button>
-                <Button size="small" color="primary">
-                    Delete Playlist
-                </Button>
-            </CardActions>
-        </Card>
-    )
-}
+const PlaylistCard = ({ playlistName }: any) => {
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary">
+            {playlistName}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          View Playlist
+        </Button>
+        <Button size="small" color="primary">
+          Delete Playlist
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
 
-export default PlaylistCard
+export default PlaylistCard;
