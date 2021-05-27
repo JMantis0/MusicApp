f70b74f825a359084158da20ddb6e012d8c5850e
jcowelch <jcowelch@gmail.com>
2021-05-26 20:47:20 -0400

    Fix tests for playlist pojo

--- backend/src/test/java/com/musicapp/model/SongModelTest.java
+++ backend/src/test/java/com/musicapp/model/SongModelTest.java
@@ -10,16 +10,11 @@ public class SongModelTest {
 
     @BeforeEach
     public void setUp(){
-        Artist artist = new Artist();
-        artist.setArtistId("Id");
-
-        Album album = new Album();
-        album.setAlbumId("Id");
 
         song = new Song();
         song.setSongId("Id");
-        song.setArtist(artist);
-        song.setAlbum(album);
+        song.setArtist("artist");
+        song.setAlbum("album");
         song.setTitle("TT");
         song.setPreview("PV");
     }
@@ -32,14 +27,14 @@ public class SongModelTest {
 
     @Test
     public void songArtistTest(){
-        Artist foundArtist = song.getArtist();
-        Assertions.assertEquals("Id",foundArtist.getArtistId());
+        String foundArtist = song.getArtist();
+        Assertions.assertEquals("artist",foundArtist);
     }
 
     @Test
     public void songAlbumTest(){
-        Album foundAlbum = song.getAlbum();
-        Assertions.assertEquals("Id",foundAlbum.getAlbumId());
+        String foundAlbum = song.getAlbum();
+        Assertions.assertEquals("album",foundAlbum);
     }
 
     @Test
